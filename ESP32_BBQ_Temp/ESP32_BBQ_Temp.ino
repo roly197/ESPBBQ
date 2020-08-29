756d168607dbcb3059a5e49021169ab3ad5fa6bd
roly197 <rolyvanleersum@ziggo.nl>
2020-08-29 11:16:07 +0200

    Combined Dash and MAX31855 test. MAX reading still not working

--- ESP32_BBQ_Temp/ESP32_BBQ_Temp.ino
+++ ESP32_BBQ_Temp/ESP32_BBQ_Temp.ino
@@ -25,22 +25,43 @@
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <ESPDash.h>
-#include "Adafruit_MAX31855.h"
+#include <Adafruit_MAX31855.h>
 
 AsyncWebServer server(80);
 
 const char* ssid = "Bigdragon"; // Your WiFi SSID
 const char* password = "Unreal!!!"; // Your WiFi Password
 
+// Creating a thermocouple instance with software SPI on any three
+// digital IO pins.
+#define MAXDO   19
+#define MAXCS   5
+#define MAXCLK  18
 
-void sliderChanged(const char* id, int value){
-   Serial.println("Slider Changed - "+String(id)+" "+String(value));
-   ESPDash.updateGaugeChart("gauge1", value);
-}
+// initialize the Thermocouple
+Adafruit_MAX31855 thermocouple(MAXCLK, MAXCS, MAXDO);
+
+// Example creating a thermocouple instance with hardware SPI
+// on a given CS pin.
+//#define MAXCS   10
+//Adafruit_MAX31855 thermocouple(MAXCS);
 
 
 void setup() {
     Serial.begin(115200);
+
+    //Setup MAX31855
+    Serial.println("MAX31855 test");
+    // wait for MAX chip to stabilize
+    delay(500);
+    Serial.print("Initializing sensor...");
+    if (!thermocouple.begin()) {
+      Serial.println("ERROR.");
+      while (1) delay(10);
+    }
+    Serial.println("DONE.");
+
+    //Setup Wifi
     WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, password);
     if (WiFi.waitForConnectResult() != WL_CONNECTED) {
@@ -49,17 +70,29 @@ void setup() {
     }
     Serial.print("IP Address: ");
     Serial.println(WiFi.localIP());
-    
-    ESPDash.init(server);   // Initiate ESPDash and attach your Async webserver instance
-    
-    // Attach Slider Change Events
-    ESPDash.attachSliderChanged(sliderChanged);
 
-    ESPDash.addSliderCard("slider1", "Gauge Slider", 3);
-    ESPDash.addGaugeChart("gauge1", "Gauge 1");
+    //Setup Dash (Web framework)
+    ESPDash.init(server);   // Initiate ESPDash and attach your Async webserver instance
+    ESPDash.addTemperatureCard("temp1", "Meat Temp", 0, 0);
     server.begin();
 }
 
 void loop() {
+    // basic readout test, just print the current temp
+   Serial.print("Internal Temp = ");
+   Serial.println(thermocouple.readInternal());
+
+   double c = thermocouple.readCelsius();
+   if (isnan(c)) {
+     Serial.println("Something wrong with thermocouple!");
+     //Debug: display temp although reading is wrong... (remove later)
+     ESPDash.updateTemperatureCard("temp1", (int) c);
+   } else {
+     Serial.print("C = ");
+     Serial.println(c);
+     ESPDash.updateTemperatureCard("temp1", (int) c);
+   }
+
+   delay(1000);
 
 }
